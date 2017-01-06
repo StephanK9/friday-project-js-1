3ddb2908eb617000d7402899b456bfdbff5bb458
StephanK9 <stephane.khoury@gmail.com>
2017-01-06 14:19:13 -0800

    add the gulp file that was made from scratch today

---
+++ js/doctor-interface.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#drsearch').submit(function(event){
+    event.preventDefault();
+    var symptom = $('#symptom').val();
+    $('#drsearch').hide();
+    $("#solution").prepend('<p>Your ' + symptom + ' has been recorded!</p>');
+  });
+});
